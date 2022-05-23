ff0c99300bdd34455da004b614765a6a35abd9ec
DecentraMindsCoin <decentraminds@gmail.com>
2022-05-22 22:06:07 -0700

    disable button styling

--- src/components/BetterCounter.js
+++ src/components/BetterCounter.js
@@ -2,32 +2,35 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 const styles = {
-  counter: ' bg-cyan-500 text-center h-56 flex relative',
+  counter: ' bg-cyan-200 text-center h-56 flex relative',
   countVal: 'absolute top-1/3 w-full mx-auto text-4xl',
   buttons: 'flex justify-between w-full px-5 pt-40',
-  incButton: 'text-white uppercase font-bold rounded-full px-5 my-3 border-4 border-white shadow-cyan-400 shadow-xl bg-black'
+  incButton:
+    'text-white uppercase font-bold rounded-full px-5 my-3 border-4 border-white shadow-cyan-400 shadow-xl bg-black',
 }
-const tw = styles;
-
-
+const tw = styles
 
 const BetterCounter = () => {
   const [count, setCount] = useState(0)
   const increment = () => {
     setCount(count + 1)
-}
-const [secondCount, setSecondCount] = useState(5);
-useEffect(() => {
+  }
+  const [secondCount, setSecondCount] = useState(5)
+  useEffect(() => {
     setCount(count + 1)
-}, [secondCount])
-console.log(count)
+  }, [secondCount])
+  console.log(count)
   return (
     <div className={tw.counter}>
-      <h1 className={tw.countVal}>{count} - {secondCount}</h1>
+      <h1 className={tw.countVal}>
+        {count} - {secondCount}
+      </h1>
       <div className={tw.buttons}>
         {/* <button className={tw.incButton} onClick={increment}>increment</button>
         <button className={tw.incButton} onClick={() => setCount(count - 1)}>decrement</button> */}
-        <button onClick={() => setSecondCount(secondCount + 1)}>Increment Second</button>
+        <button onClick={() => setSecondCount(secondCount + 1)}>
+          Increment Second
+        </button>
       </div>
     </div>
   )
--- src/components/DisableButton.js
+++ src/components/DisableButton.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { useState } from 'react'
 
+const style = {
+  wrapper: `w-full h-full mx-auto text-center bg-blue-500 py-10 space-y-5 font-mono text-xl`,
+  heading: `font-bold uppercase underline text-xl py-5`,
+  input: `border-2 border-black rounded-full w-3/4`,
+  button: `border-2 border-black bg-gray-300 px-2 py-1 rounded-full`,
+}
+
 const DisableButton = () => {
   const [value, setValue] = useState('')
 
   return (
     <>
-    <div className='w-full h-full mx-auto text-center bg-blue-500 py-10 space-y-5 font-mono text-xl'>
-             <h3 className='font-bold uppercase underline text-xl py-5'>Disable Button Challenge</h3>
-      <input className='border-2 border-black rounded-full w-3/4' type="text" onChange={(e) => setValue(e.target.value)} />
-      <button className='border-2 border-black bg-gray-300 px-2 py-1 rounded-full' disabled={value.length < 1}>Submit</button> 
-    </div>
-
+      <div className={style.wrapper}>
+        <h3 className={style.heading}>Disable Button Challenge</h3>
+        <input
+          className={style.input}
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className={style.button} disabled={value.length < 1}>
+          Submit
+        </button>
+      </div>
     </>
   )
 }
