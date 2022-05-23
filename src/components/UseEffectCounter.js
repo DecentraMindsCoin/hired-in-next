e5346ce05c279c91e155fe7eeb4215a44d68e880
DecentraMindsCoin <decentraminds@gmail.com>
2022-05-22 21:29:41 -0700

    fix tailwind styling into owm style component

--- src/components/UseEffectCounter.js
+++ src/components/UseEffectCounter.js
@@ -2,6 +2,14 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 
+
+const styles = {
+  wrapper: `relative mx-auto h-full  w-full space-y-5 bg-pink-400 text-center font-mono `,
+  container: `absolute top-1/3 w-full`,
+  heading: `underline text-xl font-bold uppercase`,
+  button: `rounded-full border-2 px-4 py-1 my-5 hover:bg-white hover:text-black `,
+
+}
 const UseEffectCounter = () => {
   const [count, setCount] = useState(0)
 
@@ -9,11 +17,11 @@ const UseEffectCounter = () => {
     document.title = `You clicked ${count} times`
   })
   return (
-    <div className="relative mx-auto h-full  w-full space-y-5 bg-pink-400 text-center font-mono ">
-      <div className="absolute top-1/3 w-full">
-        <h1 className="underline text-xl font-bold uppercase">You Clicked {count} times</h1>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>You Clicked {count} times</h1>
         <button
-          className="rounded-full border-2 px-4 py-1 my-5 hover:bg-white hover:text-black "
+          className={styles.button}
           onClick={() => setCount(count + 1)}
         >
           Click Me{' '}
