ff0b2bfc761017dc6144887f252f3de5a014514b
DecentraMindsCoin <decentraminds@gmail.com>
2022-06-21 23:52:35 -0700

    change styling to ScrollEvent component

--- src/components/ScrollEvent.js
+++ src/components/ScrollEvent.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 
+const styles = {
+  wrapper: `text-center w-full font-mono mx-auto  h-full py-7 bg-white font-bold uppercase tracking-wider italic cursor-pointer space-y-5 border-2 border-black`,
+
+  container: ` w-full flex-row space-x-3`,
+
+  button: `rounded-full border-2 px-2  py-1`,
+
+  input: `border-2 outline-none px-5 rounded-xl border-2 border-black`,
+}
 const ScrollEvent = () => {
   const [count, setCount] = useState(0)
 
-const handleScroll = () => {
+  const handleScroll = () => {
     setCount(count + scrollY)
-}
+  }
   useEffect(() => {
     window.addEventListener('scroll', handleScroll)
     return () => {
@@ -14,7 +23,7 @@ const handleScroll = () => {
     }
   })
   return (
-    <div>
+    <div className={styles.wrapper}>
       <p>You have moved {count} spots</p>
       <button onClick={() => setCount(count + 1)}>Move a spot</button>
     </div>
