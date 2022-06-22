bb139b8e7e04ad140ea70343d84cf24aef38b345
DecentraMindsCoin <decentraminds@gmail.com>
2022-06-21 23:43:11 -0700

    add wrapper style to UseRef component

--- src/components/UseRef.js
+++ src/components/UseRef.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 
 const UseRef = () => {
+  const styles = {
+    wrapper: `text-center w-full font-mono mx-auto  h-full py-7 bg-black bg-opacity-50 hover:bg-opacity-90 font-bold uppercase tracking-wider italic cursor-pointer space-y-5`,
+  }
   const [name, setName] = useState('')
   return (
-    <div>
-      <input onChange={e => setName(e.target.value)}/>
+    <div className={styles.wrapper}>
+      <input onChange={(e) => setName(e.target.value)} />
       <div>My name is {name} </div>
     </div>
   )
