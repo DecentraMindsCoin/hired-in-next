1038e089779f93f9b74504e4a26753c300f848aa
DecentraMindsCoin <decentraminds@gmail.com>
2022-06-26 21:56:11 -0700

    add FetchApi method using useEffect hook to fetch data

--- src/components/Fetch/FetchApi.js
+++ src/components/Fetch/FetchApi.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FetchApi = () => {
-    const [data, setData]= useState(null);
+  const [data, setData] = useState([])
+  const url = 'https://jsonplaceholder.typicode.com/users/'
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((apiData) => {
+        setData(apiData)
+        console.log(apiData)
+      })
+  }, [])
+
   return (
-    <div>FetchApi</div>
+    <div className="h-96 overflow-y-scroll ml-32">
+      <ul>
+        {data.map((user) => {
+          return (
+            <div className='py-5' key={user.id}>
+              <li>{user.name}</li>
+              <li>{user.address.street}</li>
+              <li>{user.email}</li>
+            </div>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
