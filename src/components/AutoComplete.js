810af7e3f4ce36466372f64f372af84badcb83cd
DecentraMindsCoin <decentraminds@gmail.com>
2022-06-26 23:58:48 -0700

    add conditional rendering version of code to AutoComplete component

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -26,6 +26,7 @@ const AutoComplete = () => {
 
   const autoComplete = (event) => {
     setValue(event.target.value)
+    
     array.forEach((name) => {
       if (
         name.substring(0, event.target.value.length).toUpperCase() ===
@@ -61,3 +62,67 @@ const AutoComplete = () => {
 }
 
 export default AutoComplete
+
+//same thing as above just using consitional rendering
+
+// import React, { useState } from 'react'
+
+// const AutoComplete = () => {
+//   let array = [
+//     'Tom',
+//     'John',
+//     'Bob',
+//     'Sarah',
+//     'Mike',
+//     'Kate',
+//     'Adam',
+//     'Chris',
+//     'Richard',
+//     'Carol',
+//     'Tim',
+//   ]
+
+//   let suggestions = []
+//   const [value, setValue] = useState()
+//   const [list, setList] = useState()
+
+//   const setInputValue = (e, name) => {
+//     setValue(name)
+//     setList(list.length === 0)
+//   }
+
+//   const autoComplete = (event) => {
+//     setValue(event.target.value)
+//     array.forEach((name) => {
+//       {
+//         name.substring(0, event.target.value.length).toUpperCase() ===
+//           event.target.value.toUpperCase() && suggestions.push(name)
+//         console.log(suggestions)
+//         setList(
+//           suggestions.map((name, index) => {
+//             return (
+//               <div>
+//                 <p key={index} onClick={(e) => setInputValue(e, name)}>
+//                   {name}
+//                 </p>
+//               </div>
+//             )
+//           })
+//         )
+//       }
+//       {
+//         event.target.value.length === 0 && setList('')
+//       }
+//     })
+//   }
+
+//   console.log(value)
+//   return (
+//     <div>
+//       <input type="text" value={value} onChange={(e) => autoComplete(e)} />
+//       <div>{list}</div>
+//     </div>
+//   )
+// }
+
+// export default AutoComplete
