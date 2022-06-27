47325434036daa838c31fdf5657e2f9e31f42b42
DecentraMindsCoin <decentraminds@gmail.com>
2022-06-26 21:53:00 -0700

    add completed login form with success modal on submit form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { login } from "../../lib/utils";
+import { useState } from 'react'
+import { login } from '../../lib/utils'
 
 // ================ LOGIN FORM ====================
 //
@@ -15,54 +15,52 @@ import { login } from "../../lib/utils";
 //  * Show an error message from the login() if login fails. The error should be cleared every time user re-attempts to log in.
 //  * Show an alert box (native Javascript alert) if login succeeds. Investigate the login function to find out how to log in successfully.
 
-
 const styles = {
-    wrapper: `w-full h-full py-5 text-center bg-emerald-300`,
-    row: `py-5 flex justify-between px-5 items-center`,
-    errorMessage: ``,
-    input: `mx-5 outline-none px-3 rounded-xl py-2`,
-    button: `border-2 mx-5 py-2 rounded-full mt-8 cursor-pointer hover:bg-white`
-
+  wrapper: `w-full h-full py-5 text-center bg-emerald-300`,
+  row: `py-5 flex justify-between px-5 items-center`,
+  errorMessage: ``,
+  input: `mx-5 outline-none px-3 rounded-xl py-2`,
+  button: `border-2 mx-5 py-2 rounded-full mt-8 cursor-pointer hover:bg-white`,
 }
 export default function LoginForm() {
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
 
-  const disableButton = !email || password.length < 6 || loading;
+  const disableButton = !email || password.length < 6 || loading
 
   const handleLogin = async () => {
     setError(null)
     setLoading(true)
     try {
       await login({ email, password })
-      alert("Login successfull")
+      alert('Login successfull')
       setLoading(false)
     } catch (error) {
       setError(error.message)
       setLoading(false)
     }
-  };
+  }
 
   return (
     <div className={styles.wrapper}>
-        <h1 className="font-bold text-xl underline">Login Form</h1>
+      <h1 className="text-xl font-bold underline">Login Form</h1>
       <div className={styles.row}>
-        <label htmlFor={"email"}>Email</label>
+        <label htmlFor={'email'}>Email</label>
         <input
-          id={"email"}
-          type={"email"}
+          id={'email'}
+          type={'email'}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           className={styles.input}
         />
       </div>
       <div className={styles.row}>
-        <label htmlFor={"password"}>Password</label>
+        <label htmlFor={'password'}>Password</label>
         <input
-          id={"password"}
-          type={"password"}
+          id={'password'}
+          type={'password'}
           value={password}
           className={styles.input}
           onChange={(e) => setPassword(e.target.value)}
@@ -74,9 +72,9 @@ export default function LoginForm() {
 
       <div className={styles.button}>
         <button disabled={disableButton} onClick={handleLogin}>
-          Login
+          <p>Login</p>
         </button>
       </div>
     </div>
-  );
+  )
 }
